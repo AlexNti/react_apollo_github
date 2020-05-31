8847521f9ffbd342e6c6a35f79de69a563903286
Alexandros <a.ntitoras@gmail.com>
2020-05-31 17:23:49 +0300

    added global styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import client from './apollo';
 import Routes from './routes';
+import GlobalStyles from './utils/GlobalStyles';
 
 function App() {
   return (
     <ApolloProvider client={client}>
+      <GlobalStyles />
       <Router>
         <Routes />
       </Router>
