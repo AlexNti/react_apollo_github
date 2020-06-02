607e0a55e7d3aa39bc263dcfbc3546256d0d8c5a
Alexandros <a.ntitoras@gmail.com>
2020-06-02 21:02:55 +0300

    added sorting

--- src/Pages/Issues/IssuesTable.jsx
+++ src/Pages/Issues/IssuesTable.jsx
@@ -3,14 +3,17 @@ import {
   Table, TableRow, TableColumn, TableHeader, TableBody, TableBodyWrapper,
 } from '../../components/Table/index';
 import getNestedObjectValue from '../../utils/getNestedObjectValue';
-
+// TODO ADD FILTER DROPDOWN
 const renderTableRow = (tableKey, data) => {
   if (!data || !data.repository) return <></>;
   const { issues } = data.repository;
   const { edges } = issues;
+  const clonedEdgesArray = [...edges];
+  const sortByCommentCount = clonedEdgesArray.sort((a, b) => b.node.comments.totalCount - a.node.comments.totalCount);
+  // const sortByCreatedAt = sortByCommentCount.sort((a, b) => new Date(a.node.createdAt) - new Date(b.node.createdAt));
 
 
-  const row = edges.map((edge) => (
+  const row = sortByCommentCount.map((edge) => (
     <TableBodyWrapper key={edge.node.id} bgColor="white">
       <TableRow>
         <TableBody>
@@ -26,7 +29,7 @@ const renderTableRow = (tableKey, data) => {
 
 const IssuesTable = ({ tableKeys = [], data }) => (
   <Table>
-    {tableKeys.map((tableKey, index) => (
+    {tableKeys.map((tableKey) => (
       <TableColumn key={tableKey.header}>
         <TableRow>
           <TableHeader>{tableKey.header}</TableHeader>
@@ -34,8 +37,6 @@ const IssuesTable = ({ tableKeys = [], data }) => (
         {renderTableRow(tableKey, data)}
       </TableColumn>
     ))}
-
-
   </Table>
 );
 
